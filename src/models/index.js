074c98359cf621a4357b8d95b40d6d94131c2b2f
NSALAA9 <alaansairat99@gmail.com>
2023-07-03 21:29:44 +0000

    tests

---
+++ src/models/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const { Sequelize, DataTypes } = require("sequelize");
+
+const DATABASE_URL = process.env.NODE_ENV === 'test' ? 'sqlite:memory:' : process.env.DATABASE_URI;
+const sequelizeOptions = process.env.NODE_ENV === 'production'
+  ? {
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      }
+    }
+  : {};
+
+const sequelize = new Sequelize(DATABASE_URL, sequelizeOptions);
+
+const Food = require('./food.model')(sequelize, DataTypes);
+
+module.exports = {
+  db: sequelize,
+  Food: Food,
+};
